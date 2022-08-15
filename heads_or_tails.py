0684b45aa61c8de600a858d94b13b2c399560339
alba-molina-nyc <avmanyc@gmail.com>
2022-08-15 14:44:12 -0400

    coin tosser

---
+++ heads_or_tails.py
@@ -0,0 +1,29 @@
+"""
+Instructions
+You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+
+Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+
+There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
+
+e.g. 1 means Heads 0 means Tails
+
+Example Output
+Heads
+or
+
+Tails
+Solution"""
+import random
+def coin_tosser():
+
+    coin = random.randint(0, 1)
+
+    if coin == 0:
+        print('Heads')
+    else:
+        print('Tails')
+
+
+if __name__ == '__main__':
+    coin_tosser()
---
+++ random_module.py
@@ -0,0 +1,10 @@
+import random
+
+
+# randint(a,b) return integer between a and b
+
+random_integer = random.randint(1, 10)
+print(f'{random_integer}')
+
+
+
