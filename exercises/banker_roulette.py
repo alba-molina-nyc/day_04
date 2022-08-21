2cdc4565f537b6feb035afdcc0a523fbbb93a8dd
alba-molina-nyc <avmanyc@gmail.com>
2022-08-21 10:34:07 -0400

    another list exercise

--- exercises/banker_roulette.py
+++ exercises/banker_roulette.py
@@ -4,7 +4,7 @@ def banker_roulette():
     names_string = input('Give me everybodys names separated by commas: ')
     names = names_string.split(', ')
     num_items = len(names)
-    random_choice = random.randint(0,num_items - 1)
+    random_choice = random.randint(0, num_items - 1)
     person_who_will_pay = random.choice(names)
     print(person_who_will_pay + "is going to buy the meal today")
 
