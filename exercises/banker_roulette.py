3ac444210fc0ddcc4f06d41b1578203af34097c8
alba-molina-nyc <avmanyc@gmail.com>
2022-08-21 10:28:09 -0400

    finishes banker routlle

--- exercises/banker_roulette.py
+++ exercises/banker_roulette.py
@@ -1,10 +1,11 @@
 import random
 
 def banker_roulette():
-    names = input('Give me everybodys names separated by commas: ')
+    names_string = input('Give me everybodys names separated by commas: ')
+    names = names_string.split(', ')
     num_items = len(names)
-    random_choice = random.randint(0,num_items-1)
-    person_who_will_pay = names[random_choice]
+    random_choice = random.randint(0,num_items - 1)
+    person_who_will_pay = random.choice(names)
     print(person_who_will_pay + "is going to buy the meal today")
 
 if __name__ == '__main__':
