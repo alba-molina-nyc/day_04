671fdd16653263b13e4468716c614886e0f88aa3
alba-molina-nyc <avmanyc@gmail.com>
2022-08-21 10:24:02 -0400

    banker roulette

--- exercises/banker_roulette.py
+++ exercises/banker_roulette.py
@@ -1,13 +1,11 @@
+import random
+
 def banker_roulette():
     names = input('Give me everybodys names separated by commas: ')
-    print(f'{names}')
-    print(type(names))
-    
-    names = names.split(', ')
-    print(f'{names}')
-    print(type(names))
-    pass
-
+    num_items = len(names)
+    random_choice = random.randint(0,num_items-1)
+    person_who_will_pay = names[random_choice]
+    print(person_who_will_pay + "is going to buy the meal today")
 
 if __name__ == '__main__':
     banker_roulette()
